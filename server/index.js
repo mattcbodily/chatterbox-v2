790fd6e57c058f1cbc51fcdeb1310ba20056d8cb
mattcbodily <mattcbodily@gmail.com>
2020-03-14 13:04:16 -0600

    authentication endpoints created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express'),
       massive = require('massive'),
+      session = require('express-session'),
       authCtrl = require('./controllers/authController'),
       mainCtrl = require('./controllers/mainController'),
       {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
@@ -9,6 +10,13 @@ const express = require('express'),
 
 app.use(express.json());
 
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+}))
+
 massive({
     connectionString: CONNECTION_STRING,
     ssl: {rejectUnauthorized: false}
@@ -16,5 +24,10 @@ massive({
     app.set('db', db);
     console.log('db connected')
 })
+
+//auth
+app.post('/api/login', authCtrl.login);
+app.post('/api/register', authCtrl.register);
+app.get('/api/logout', authCtrl.logout);
 
 app.listen(port, () => console.log(`Server running on ${port}`));
