802c8db5a06d6ee2b5dd7b1d81dff8c5e8e08f82
mattcbodily <mattcbodily@gmail.com>
2020-03-18 16:26:39 -0600

    added slide animation for setting menu

--- src/Components/SettingMenu/SettingMenu.js
+++ src/Components/SettingMenu/SettingMenu.js
@@ -2,5 +2,7 @@ import React from 'react';
 import './SettingMenu.css';
 
 export default props => (
-    <div className='setting-menu'>Setting Menu</div>
+    <div className='setting-menu'>
+        
+    </div>
 )
